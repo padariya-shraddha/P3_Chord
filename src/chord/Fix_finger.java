ee7bd536bc0e5ea9d9718c4db686361c083a473b
shraddha padariya <padariya.shraddha@gmail.com>
2017-06-09 09:52:28 -0700

    some changes
    some changes

--- src/chord/Fix_finger.java
+++ src/chord/Fix_finger.java
@@ -38,7 +38,6 @@ public class Fix_finger extends Thread{
 		  try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	  }
